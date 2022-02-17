d040fac17636750edea1a2e2069411da995808fc
kimkw88 <55889947+kimkw88@users.noreply.github.com>
2022-02-16 16:20:44 -0800

    Update test.py

--- parser/test.py
+++ parser/test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from parser import Parser
+from categorizer import Categorizer
 
 file1 = 'test_csv_files/sample.csv'
 file2 = 'test_csv_files/(alternate_values)farm_with_a_little_of_everything.xlsx'
@@ -9,9 +10,13 @@ file5 = 'test_csv_files/(no_category)farm_with_a_little_of_everything.xlsx'
 file6 = 'test_csv_files/(no_space)farm_with_a_little_of_everything.xlsx'
 file7 = 'test_csv_files/(swapped_columns)farm_with_a_little_of_everything.xlsx'
 
-cat_name = 'Category' # file1, file2, file4, file6, file7
+cat_name = 'Category' # file1, file3, file4, file6, file7
 # cat_name = 'CaTeGoRy' # file2
 # cat_name = 'Tags' # file5
-p = Parser(file1, cat_name)
+cat_name = cat_name.lower().title()
+p = Parser(file3, cat_name)
 
 p.run()
+
+c = Categorizer()
+c.extract_cat_name(cat_name, p.cat_dict)

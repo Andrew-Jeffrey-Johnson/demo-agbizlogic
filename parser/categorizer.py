d2c140d1620340341af5f10a9b49988cf74710b6
kimkw88 <55889947+kimkw88@users.noreply.github.com>
2022-02-21 13:50:34 -0800

    updated

--- parser/categorizer.py
+++ parser/categorizer.py
@@ -1,34 +1,48 @@
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn import svm
 
-class Categorizer:
-    # def dictionary(self, df):
-    #     for i in df.keys():
-    #         for x in df[i]:
-    #             print(x)
-    #             print()
-    #         print('='*100)
-    #     print("dictionary found")
+class Categorizer:           
+     
+    def train(test_data, df, dictionary):
+        train_x = df['Tags'].tolist()
+        train_y = df['Category'].tolist()
+        vectorizer = CountVectorizer(binary=True)
+        train_x_vectors = vectorizer.fit_transform(train_x)        
+        clf = svm.SVC(kernel='linear')
+        clf.fit(train_x_vectors, train_y)
+ 
+        categories = []
+        for key, data in dictionary.items():
+            for data in dictionary[key]:
+                if 'Category' not in data:
+                    categories.append(data['Tags'])
+                else:
+                    categories.append(data['Category'])
+        predicted = [list(clf.predict(vectorizer.transform([data]))) for data in test_data]    
+        df = pd.DataFrame(list(zip(test_data, categories[1:], np.array(predicted).flatten())), columns=['cat_name','Category','Predicted Category'])
+        print("Predicted data:\n",df)
+        df.to_csv('predicted_categories.csv')
     
-    def extract_cat_name(self, cat_name, df):
-        result = []
-        # key ---> Ag Program Payments, Car & Truck Expense, ...
+    def load_train_data(test_data, dictionary):
+        df = pd.read_csv('tags_combination.csv', header=None)
+        df = pd.DataFrame(df).dropna(how='all')
+        header = df.iloc[0].str.lower().str.title()
+        df = df[1:]
+        df.rename(columns=header, inplace=True)
+        df = df[['Tags','Category']].drop_duplicates().dropna()
+        print("Train data:\n", df)
+        print('='*100)
+        df.reset_index(inplace=True, drop=True)
+        Categorizer.train(test_data, df, dictionary)
+        
+    def extract_cat_name(self, cat_name, df):        
+        extracted = []
         for key, data in df.items():
             for data in df[key]:
-                # remove duplicates and store only one value
-                if data[cat_name] not in result:
-                    result.append(data[cat_name])
-        # remove nan
-        cat_name_data = [item for item in result if not(pd.isnull(item)) == True]
-        tfidf_vectorizer = TfidfVectorizer()
-        tfidf_vectorizer.fit(cat_name_data)
-        print("cat_name_data:\n", cat_name_data)
-        print()
-        print("tfidf_vectorizer.get_feature_names_out():\n", tfidf_vectorizer.get_feature_names_out())
-        print()
-        print("tfidf_vectorizer.vocabulary:\n", tfidf_vectorizer.vocabulary_)
-        print()
-        print("tfidf_vectorizer.transform:\n", tfidf_vectorizer.transform(cat_name_data).toarray())
-        # cat_name_data_vectors = tfidf_vectorizer.transform(cat_name_data).toarray()
-        # np.savetxt('cat_data.csv', cat_name_data_vectors, fmt='%s', delimiter=',')
+                if (pd.isnull(data[cat_name]) == True):
+                    data[cat_name] = 'Unknown'
+                extracted.append(data[cat_name])
+        Categorizer.load_train_data(extracted[1:], df)
+        
--- parser/test.py
+++ parser/test.py
@@ -10,11 +10,11 @@ file5 = 'test_csv_files/(no_category)farm_with_a_little_of_everything.xlsx'
 file6 = 'test_csv_files/(no_space)farm_with_a_little_of_everything.xlsx'
 file7 = 'test_csv_files/(swapped_columns)farm_with_a_little_of_everything.xlsx'
 
-cat_name = 'Category' # file1, file3, file4, file6, file7
+# cat_name = 'Category' # file1, file3, file4, file6, file7
 # cat_name = 'CaTeGoRy' # file2
-# cat_name = 'Tags' # file5
+cat_name = 'Tags' # file5
 cat_name = cat_name.lower().title()
-p = Parser(file3, cat_name)
+p = Parser(file1, cat_name)
 
 p.run()
 
